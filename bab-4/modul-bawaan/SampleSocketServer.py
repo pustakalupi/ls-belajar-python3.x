ca887e7ff293d8068e26be49fbd24b3e40b8f9c3
pustakalupi <lusfikars@gmail.com>
2018-09-27 10:07:49 +0700

    update

--- bab-4/modul-bawaan/SampleSocketServer.py
+++ bab-4/modul-bawaan/SampleSocketServer.py
@@ -4,15 +4,17 @@ sockServer = socket.socket()
 sockServer.bind(("127.0.0.1",7000))
 
 sockServer.listen(1)
-sockClient, address = sockServer.accept()
-print ("ada koneksi dari: " + str(address))
 
 while True:
-        data = sockClient.recv(1024).decode()
-        if not data:
-                break
-        print ("data dari client: " + str(data))
-        print ("kirim balik: " + str(data))
-        sockClient.send(data.encode())
+        sockClient, address = sockServer.accept()
+        print ("ada koneksi dari: " + str(address))
 
-sockClient.close()
+        while True:
+                data = sockClient.recv(1024).decode()
+                if not data:
+                        break
+                print ("data dari client: " + str(data))
+                print ("kirim balik: " + str(data))
+                sockClient.send(data.encode())
+
+        sockClient.close()

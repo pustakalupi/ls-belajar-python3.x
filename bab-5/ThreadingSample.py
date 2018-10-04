9d51916db2a57bb798b4eee81f5bea5a32a4cb90
pustakalupi <lusfikars@gmail.com>
2018-10-05 04:36:08 +0700

    update

--- bab-5/ThreadingSample.py
+++ bab-5/ThreadingSample.py
@@ -5,23 +5,22 @@ import threading
 import time
 
 class PenampungThreading(threading.Thread):
-    def __init__(self, threadID, name, delay, berapaKali):
+    def __init__(self, nama, delay, berapaKali):
         threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
+        self.nama = nama
         self.delay = delay
         self.berapaKali = berapaKali
 
     def run(self):
-        print ("Thread Dimulai: " + self.name)
+        print ("Thread Dimulai: " + self.nama)
         while self.berapaKali:
             time.sleep(self.delay)
-            print ("%s: %s" % ( self.name, str(self.berapaKali) ))
+            print ("%s: %s" % ( self.nama, str(self.berapaKali) ))
             self.berapaKali -= 1
-        print ("Thread Selesai: " + self.name)
+        print ("Thread Selesai: " + self.nama)
 
-th1 = PenampungThreading(1, "Thread-1", 2, 5)
-th2 = PenampungThreading(2, "Thread-2", 1, 6)
+th1 = PenampungThreading("Thread-1", 2, 5)
+th2 = PenampungThreading("Thread-2", 1, 6)
 
 th1.start()
 th2.start()
